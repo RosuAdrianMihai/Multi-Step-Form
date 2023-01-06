5aab309b72d7ded5dce3017d834cbb523feab804
Mihai Rosu <rosumihai16@gmail..com>
2023-01-06 22:16:58 +0200

    Modified head place

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Multi Step Form</title>
+        <meta name="viewport" content="height=100%, width=100%" />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 
-import Head from "next/head";
-
 import RootProvider from "../context/RootProvider";
 import Card from "../components/Card/Card";
 
 function Index() {
   return (
     <>
-      <Head>
-        <title>Multi Step Form</title>
-        <meta name="viewport" content="minimum-scale=1" />
-      </Head>
-
       <div className="sm:flex sm:items-center sm:justify-center sm:h-screen">
         <RootProvider>
           <Card />
