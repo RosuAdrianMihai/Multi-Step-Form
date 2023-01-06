50a8a0a4f103758c1f24613898ce819d8714cd16
Mihai Rosu <rosumihai16@gmail..com>
2023-01-06 22:04:51 +0200

    Head added

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 
+import Head from "next/head";
+
 import RootProvider from "../context/RootProvider";
 import Card from "../components/Card/Card";
 
 function Index() {
   return (
-    <div className="sm:flex sm:items-center sm:justify-center sm:h-screen">
-      <RootProvider>
-        <Card />
-      </RootProvider>
-    </div>
+    <>
+      <Head>
+        <title>Multi Step Form</title>
+        <meta name="viewport" content="minimum-scale=1" />
+      </Head>
+
+      <div className="sm:flex sm:items-center sm:justify-center sm:h-screen">
+        <RootProvider>
+          <Card />
+        </RootProvider>
+      </div>
+    </>
   );
 }
 
