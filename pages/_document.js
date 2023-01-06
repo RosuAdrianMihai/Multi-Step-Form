356d3766b98b15f1e20df19b1f7479f7e99a329b
Mihai Rosu <rosumihai16@gmail..com>
2023-01-06 22:24:00 +0200

    revert head

--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Multi Step Form</title>
-        <meta name="viewport" content="height=100%, width=100%" />
-      </Head>
+      <Head />
 
       <body>
         <Main />
