ad58832868c87b4d365d428433f84a203fe8089f
Mihai Rosu <rosumihai16@gmail..com>
2023-01-06 21:52:43 +0200

    first page centering

--- components/Steps/Step1.js
+++ components/Steps/Step1.js
@@ -63,131 +63,135 @@ function Step1() {
   }
 
   return (
-    <section>
-      <h1 className="mb-1 text-3xl font-bold text-marineBlue">Personal info</h1>
-      <p className="mb-6 text-coolGray">
-        Please provide your name, email address, and phone number.
-      </p>
+    <section className="flex items-center justify-center">
+      <div>
+        <h1 className="mb-1 text-3xl font-bold text-marineBlue">
+          Personal info
+        </h1>
+        <p className="mb-6 text-coolGray">
+          Please provide your name, email address, and phone number.
+        </p>
 
-      <div className="mb-4">
-        <div className="flex justify-between mb-2">
-          <label htmlFor="name" className="text-marineBlue">
-            Name
-          </label>
-          {errorName &&
-            errorElement("name", userData.name.value, userData.name.valid)}
+        <div className="mb-4">
+          <div className="flex justify-between mb-2">
+            <label htmlFor="name" className="text-marineBlue">
+              Name
+            </label>
+            {errorName &&
+              errorElement("name", userData.name.value, userData.name.valid)}
+          </div>
+
+          <input
+            value={userData.name.value}
+            onChange={(event) => {
+              const fieldObj = {
+                value: event.currentTarget.value,
+                valid: event.currentTarget.value.trim().length > 4,
+              };
+
+              dispatchUserData({ type: "field", field: "name", fieldObj });
+
+              if (fieldObj.valid === false) {
+                const timeoutId = setTimeout(() => {
+                  setErrorName(true);
+                }, 3000);
+
+                return () => {
+                  clearTimeout(timeoutId);
+                };
+              }
+            }}
+            type="text"
+            id="name"
+            placeholder="e.g Stephen King"
+            className={`w-full h-12 pl-4 rounded-lg outline outline-1 focus:outline-2 font-bolder ${
+              errorName && !userData.name.valid ? "outline-strawberryRed" : ""
+            }`}
+          />
         </div>
 
-        <input
-          value={userData.name.value}
-          onChange={(event) => {
-            const fieldObj = {
-              value: event.currentTarget.value,
-              valid: event.currentTarget.value.trim().length > 4,
-            };
+        <div className="mb-4">
+          <div className="flex justify-between mb-2">
+            <label htmlFor="email" className="text-marineBlue">
+              Email Address
+            </label>
+            {errorEmail &&
+              errorElement("email", userData.email.value, userData.email.valid)}
+          </div>
 
-            dispatchUserData({ type: "field", field: "name", fieldObj });
-
-            if (fieldObj.valid === false) {
-              const timeoutId = setTimeout(() => {
-                setErrorName(true);
-              }, 3000);
-
-              return () => {
-                clearTimeout(timeoutId);
+          <input
+            value={userData.email.value}
+            onChange={(event) => {
+              const fieldObj = {
+                value: event.currentTarget.value,
+                valid: event.currentTarget.value.match(emailRegex),
               };
-            }
-          }}
-          type="text"
-          id="name"
-          placeholder="e.g Stephen King"
-          className={`w-full h-12 pl-4 rounded-lg outline outline-1 focus:outline-2 font-bolder ${
-            errorName && !userData.name.valid ? "outline-strawberryRed" : ""
-          }`}
-        />
-      </div>
 
-      <div className="mb-4">
-        <div className="flex justify-between mb-2">
-          <label htmlFor="email" className="text-marineBlue">
-            Email Address
-          </label>
-          {errorEmail &&
-            errorElement("email", userData.email.value, userData.email.valid)}
+              dispatchUserData({ type: "field", field: "email", fieldObj });
+
+              if (fieldObj.valid === false) {
+                const timeoutId = setTimeout(() => {
+                  setErrorEmail(true);
+                }, 3000);
+
+                return () => {
+                  clearTimeout(timeoutId);
+                };
+              }
+            }}
+            type="email"
+            id="email"
+            placeholder="e.g stephenking@lorem.com"
+            className={`w-full h-12 pl-4 rounded-lg outline outline-1 focus:outline-2 font-bolder ${
+              errorEmail && !userData.email.valid ? "outline-strawberryRed" : ""
+            }`}
+          />
         </div>
 
-        <input
-          value={userData.email.value}
-          onChange={(event) => {
-            const fieldObj = {
-              value: event.currentTarget.value,
-              valid: event.currentTarget.value.match(emailRegex),
-            };
+        <div className="mb-4">
+          <div className="flex justify-between mb-2">
+            <label htmlFor="phone" className="text-marineBlue">
+              Phone Number
+            </label>
+            {errorPhone &&
+              errorElement("phone", userData.phone.value, userData.phone.valid)}
+          </div>
 
-            dispatchUserData({ type: "field", field: "email", fieldObj });
-
-            if (fieldObj.valid === false) {
-              const timeoutId = setTimeout(() => {
-                setErrorEmail(true);
-              }, 3000);
-
-              return () => {
-                clearTimeout(timeoutId);
+          <input
+            value={userData.phone.value}
+            onChange={(event) => {
+              const fieldObj = {
+                value: event.currentTarget.value,
+                valid: event.currentTarget.value.match(phoneRegex),
               };
-            }
-          }}
-          type="email"
-          id="email"
-          placeholder="e.g stephenking@lorem.com"
-          className={`w-full h-12 pl-4 rounded-lg outline outline-1 focus:outline-2 font-bolder ${
-            errorEmail && !userData.email.valid ? "outline-strawberryRed" : ""
-          }`}
-        />
-      </div>
 
-      <div className="mb-4">
-        <div className="flex justify-between mb-2">
-          <label htmlFor="phone" className="text-marineBlue">
-            Phone Number
-          </label>
-          {errorPhone &&
-            errorElement("phone", userData.phone.value, userData.phone.valid)}
+              dispatchUserData({ type: "field", field: "phone", fieldObj });
+
+              if (fieldObj.valid === false) {
+                const timeoutId = setTimeout(() => {
+                  setErrorPhone(true);
+                }, 3000);
+
+                return () => {
+                  clearTimeout(timeoutId);
+                };
+              }
+            }}
+            type="tel"
+            id="phone"
+            placeholder="e.g +1 234 567 890"
+            className={`w-full h-12 pl-4 rounded-lg outline outline-1 focus:outline-2 font-bolder ${
+              errorPhone && !userData.phone.valid ? "outline-strawberryRed" : ""
+            }`}
+          />
         </div>
 
-        <input
-          value={userData.phone.value}
-          onChange={(event) => {
-            const fieldObj = {
-              value: event.currentTarget.value,
-              valid: event.currentTarget.value.match(phoneRegex),
-            };
+        <div className="hidden sm:items-center sm:justify-end sm:mt-16 sm:flex">
+          <NextButton validate={validateInput} />
+        </div>
 
-            dispatchUserData({ type: "field", field: "phone", fieldObj });
-
-            if (fieldObj.valid === false) {
-              const timeoutId = setTimeout(() => {
-                setErrorPhone(true);
-              }, 3000);
-
-              return () => {
-                clearTimeout(timeoutId);
-              };
-            }
-          }}
-          type="tel"
-          id="phone"
-          placeholder="e.g +1 234 567 890"
-          className={`w-full h-12 pl-4 rounded-lg outline outline-1 focus:outline-2 font-bolder ${
-            errorPhone && !userData.phone.valid ? "outline-strawberryRed" : ""
-          }`}
-        />
+        <MobileButtons validate={validateInput} />
       </div>
-
-      <div className="hidden sm:items-center sm:justify-end sm:mt-16 sm:flex">
-        <NextButton validate={validateInput} />
-      </div>
-
-      <MobileButtons validate={validateInput} />
     </section>
   );
 }
